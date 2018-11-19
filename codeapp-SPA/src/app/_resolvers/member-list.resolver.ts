f281314fde14b14184dfde4b79a5225732772d48
stephanos48 <stephanos.english@gmail.com>
2018-11-18 23:23:02 -0600

    End of Section 14

--- codeapp-SPA/src/app/_resolvers/member-list.resolver.ts
+++ codeapp-SPA/src/app/_resolvers/member-list.resolver.ts
@@ -6,14 +6,16 @@ import { AlertifyService } from '../_services/alertify.service';
 import { Observable, of } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
-
 @Injectable()
 export class MemberListResolver implements Resolve<User[]> {
+    pageNumber = 1;
+    pageSize = 5;
+
     constructor(private userService: UserService,
         private router: Router, private alertify: AlertifyService) {}
 
     resolve(route: ActivatedRouteSnapshot): Observable<User[]> {
-        return this.userService.getUsers().pipe(
+        return this.userService.getUsers(this.pageNumber, this.pageSize).pipe(
             catchError(error => {
                 this.alertify.error('Problem retrieving data');
                 this.router.navigate(['/home']);
--- codeapp.API/Data/IDatingRepository.cs
+++ codeapp.API/Data/IDatingRepository.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using codeapp.API.Helpers;
 using codeapp.API.Models;
 
 namespace codeapp.API.Data
@@ -9,10 +10,10 @@ namespace codeapp.API.Data
          void Add<T>(T entity) where T: class;
          void Delete<T>(T entity) where T: class;
          Task<bool> SaveAll();
-         Task<IEnumerable<User>> GetUsers();
+         // Task<IEnumerable<User>> GetUsers(); commented out when we did pagedList
+         Task<PagedList<User>> GetUsers(UserParams userParams);
          Task<User> GetUser(int id);
          Task<Photo> GetPhoto(int id);
          Task<Photo> GetMainPhotoForUser(int userId);
-        Task GetUser();
     }
 }
